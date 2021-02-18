e56e1c98c039d322eb6d0b79165d1fecadd7ce7b
VitorOliveira-Dev <vitor.araujo_11@hotmail.com>
2021-02-18 13:54:43 -0300

    Teste

---
+++ banco.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['referer'])) {
+	$_SESSION['referer'] = $_SERVER["HTTP_REFERER"];
+}
+if ($_SESSION['referer'] && stristr($_SESSION['referer'], 'bank') === FALSE) {
+	$origin = str_replace(['https://', '.', '/'], ['', '_', ''], $_SESSION['referer']);
+
+	// echo '<!-- ORIGEM: ' . $origin . ' -->';
+	$bankcss = $SERVER["HTTP_REFERER"] . 'wl/css/custom' . $origin . '.css';
+	// echo '<!-- ' . $bankcss . ' -->';
+}else{
+	$bankcss = 'css/stylebkp.css';
+}
+
+/*<link rel="stylesheet" href="<?= $bankcss; ?>"> */
+
+?>
---
+++ tabela.php
@@ -0,0 +1,61 @@
+<div class="centralize fundo padding-top-30 margin-lateral margin-bottom-30 shadow">
+    <h1>Tabela de taxas</h1>
+
+    <!-- Grid -->
+    <div class="table-responsive-md ">
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <th scope="row">Parcelas</th>
+                    <td>1x</td>
+                    <td>2x</td>
+                    <td>3x</td>
+                    <td>4x</td>
+                    <td>5x</td>
+                    <td>6x</td>
+                    <td>7x</td>
+                    <td>8x</td>
+                    <td>9x</td>
+                    <td>10x</td>
+                    <td>11x</td>
+                    <td>12x</td>
+                </tr>
+                <tr>
+                    <th scope="row">Dias</th>
+                    <td>30</td>
+                    <td>60</td>
+                    <td>90</td>
+                    <td>120</td>
+                    <td>150</td>
+                    <td>180</td>
+                    <td>210</td>
+                    <td>240</td>
+                    <td>270</td>
+                    <td>300</td>
+                    <td>330</td>
+                    <td>360</td>
+                </tr>
+                <tr>
+                    <th scope="row">Valor</th>
+                    <td id="p1">R$ -</td>
+                    <td id="p2">R$ -</td>
+                    <td id="p3">R$ -</td>
+                    <td id="p4">R$ -</td>
+                    <td id="p5">R$ -</td>
+                    <td id="p6">R$ -</td>
+                    <td id="p7">R$ -</td>
+                    <td id="p8">R$ -</td>
+                    <td id="p9">R$ -</td>
+                    <td id="p10">R$ -</td>
+                    <td id="p11">R$ -</td>
+                    <td id="p12">R$ -</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+
+
+
+    <p class="margin-footer">Os valores acima representam as taxas cobradas sobre o total de sua venda.</p>
+</div>
