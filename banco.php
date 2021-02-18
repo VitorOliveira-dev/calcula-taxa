5602b8a34e092cdf6133acc578c681fca30d527a
VitorOliveira-Dev <vitor.araujo_11@hotmail.com>
2021-02-18 14:14:53 -0300

    Commit on master

--- banco.php
+++ banco.php
@@ -4,7 +4,7 @@ session_start();
 if (!isset($_SESSION['referer'])) {
 	$_SESSION['referer'] = $_SERVER["HTTP_REFERER"];
 }
-if ($_SESSION['referer'] && stristr($_SESSION['referer'], 'bank') === FALSE) {
+if ($_SESSION['referer'] && stristr($_SESSION['referer'], 'bank') === TRUE) {
 	$origin = str_replace(['https://', '.', '/'], ['', '_', ''], $_SESSION['referer']);
 
 	// echo '<!-- ORIGEM: ' . $origin . ' -->';
